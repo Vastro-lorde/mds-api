520fe53ef6b7290a5db06003f0781ea163bae063
Nobert Jakpor <jakpornobert@gmail.com>
2022-08-21 20:55:35 +0100

    added auth and id checks to soccerteam routes

--- routes/socccerTeamRoutes.js
+++ routes/socccerTeamRoutes.js
@@ -5,16 +5,18 @@ const { newPlayer,
     updateTeamInfo,
     getPlayers,
     getSinglePlayer } = require("../controllers/soccerControllers")
+const { auth, idcheck } = require('../middlewares/auth')
+
 
 
 // Player routes
-router.post("/api/add_player", newPlayer)
-router.put("/api/update_player/:player", updatePlayerInfo)
-router.delete("/api/delete_player/:player", deletePlayer)
+router.post("/api/add_player", auth, idcheck, newPlayer)
+router.put("/api/update_player/:player", auth, idcheck, updatePlayerInfo)
+router.delete("/api/delete_player/:player", auth, idcheck, deletePlayer)
 router.get("/api/getAll_players", getPlayers)
 router.get("/api/getSingle_player/:player", getSinglePlayer)
 
 // Team routes
-router.put("/api/updateTeam_info", updateTeamInfo)
+router.put("/api/updateTeam_info", auth, idcheck, updateTeamInfo)
 
 module.exports = router
