106b29a6002c85beebd43e5c8049af7e8226b9f9
Nobert Jakpor <jakpornobert@gmail.com>
2022-08-22 11:05:36 +0100

    format

--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -4,18 +4,18 @@ const { signup,
     getActiveStudents,
     getInactiveStudents,
     getStudent,
-    deleteStudent } = require("../controllers/studentController") 
+    deleteStudent } = require("../controllers/studentController")
 const { studentAuth, staffAuth, idcheck } = require('../middlewares/auth');
 
 
-    const router = require("express").Router()
+const router = require("express").Router()
 
-    router.post("/login", login)
-    router.post("/signup", signup)
-    router.put("/update", staffAuth, idcheck, update)
-    router.get("getactivestudents", getActiveStudents)
-    router.get("/getinactivestudents", getInactiveStudents)
-    router.get("/getstudent", getStudent)
-    router.delete("/deleteStudent", staffAuth, idcheck, deleteStudent)
+router.post("/login", login)
+router.post("/signup", signup)
+router.put("/update", staffAuth, idcheck, update)
+router.get("getactivestudents", getActiveStudents)
+router.get("/getinactivestudents", getInactiveStudents)
+router.get("/getstudent", getStudent)
+router.delete("/deleteStudent", staffAuth, idcheck, deleteStudent)
 
-    module.exports = router
+module.exports = router
